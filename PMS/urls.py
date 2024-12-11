85ff4c4ed3712b5af8a74f1181dd0da6e2cd2ca8
Great-special <72235177+Great-special@users.noreply.github.com>
2024-12-11 14:09:54 +0100

    Update urls.py

--- PMS/urls.py
+++ PMS/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import index_views, home_views, homepage
 
 
@@ -25,3 +27,5 @@ urlpatterns = [
     path('', homepage, name='index'),
     path('home/', home_views, name='home')
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
