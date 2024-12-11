d32a9f38db05bf010b574bad3e00469c0e7ee92d
Great-special <72235177+Great-special@users.noreply.github.com>
2024-12-11 14:11:41 +0100

    Update settings.py

--- PMS/settings.py
+++ PMS/settings.py
@@ -84,10 +84,10 @@ WSGI_APPLICATION = 'PMS.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
